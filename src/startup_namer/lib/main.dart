1d2e60cb9fe4568262e8e079abe6b3a860679753
Javerage <joel.chuc@gmail.com>
2022-03-21 11:36:49 -0600

    remove debug legend in app...

--- src/startup_namer/lib/main.dart
+++ src/startup_namer/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Startup Namer App',
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
